2bbb78ceab848dca485aa78d98e7eb9d2b376e10
Andrew Martens <andrew@commandwear.com>
2015-07-22 11:06:56 -0700

    Add event emitter, quiet noisy logging, add option for default filter after login

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var net = require('net');
 var Packet = require('./packet');
+var events = require('events');
+var util = require('util');
 
-var APRS = function() {
+function APRS() {
+  if (!(this instanceof APRS)) return new APRS();
+
   this.state = 'disconnected';
   this.username = null;
   this.passcode = null;
@@ -9,26 +13,36 @@ var APRS = function() {
   this.serverSoftwareName = null;
   this.serverSoftwareVersion = null;
   this.socket = null;
-};
 
+  events.EventEmitter.call(this);
+}
 
-APRS.prototype.connect = function(user, pass) {
+util.inherits(APRS, events.EventEmitter);
+
+APRS.prototype.connect = function(user, pass, options) {
   this.username = user;
   this.passcode = pass || null;
+  this.host = options.host || 'noam.aprs2.net'
+  this.port = options.port || 14580;
+  this.debug = options.debug || false;
+  this.defaultFilter = options.filter || null;
+
   var _this = this;
   var socket = net.connect({
-    host: 'noam.aprs2.net',
-    port: 14580
+    host: this.host,
+    port: this.port
   });
   socket.on('connect', function() {
     _this.socket = this;
     _this.changeState('connected');
+    _this.emit('connected');
   });
   socket.on('end', function() {
     _this.socket = null;
     _this.serverSoftwareName = null;
     _this.serverSoftwareVersion = null;
     _this.changeState('disconnected');
+    _this.emit('disconnected');
   });
   socket.on('data', _this.parse.bind(_this));
 };
@@ -38,8 +52,13 @@ APRS.prototype.disconnect = function() {
 };
 
 APRS.prototype.changeState = function(state) {
-  console.log('State changed:', state);
+  if (this.debug) {
+    console.log('State changed:', state);
+  }
   this.state = state;
+  if ((this.state == 'verified,') && (this.defaultFilter)) {
+    this.filter(this.defaultFilter);
+  }
 };
 
 APRS.prototype.parse = function(data) {
@@ -57,19 +76,24 @@ APRS.prototype.parse = function(data) {
     }
   } else {
     var packet = new Packet(data);
-    console.log('From:', packet.sourceAddress);
-    console.log('Dest:', packet.destinationAddress);
-    console.log('Data:', packet.payload);
-    if(packet.time) {
-      console.log('Time:', packet.time);
-    } else {
-      console.log('Time: NONE');
+
+    if (this.debug) {
+      console.log('From:', packet.sourceAddress);
+      console.log('Dest:', packet.destinationAddress);
+      console.log('Data:', packet.payload);
+      if(packet.time) {
+        console.log('Time:', packet.time);
+      } else {
+        console.log('Time: NONE');
+      }
+      if(packet.latitude) {
+        console.log(' Pos:', packet.latitude, packet.longitude);
+      } else {
+        console.log(' Pos: NONE');
+      }
     }
-    if(packet.latitude) {
-      console.log(' Pos:', packet.latitude, packet.longitude);
-    } else {
-      console.log(' Pos: NONE');
-    }
+
+    this.emit('packet', packet);
   }
 };
 
@@ -81,6 +105,7 @@ APRS.prototype.filter = function(filter) {
     filterstring = filter;
   }
   var packet = '# filter ' + filterstring;
+  console.log('Adding filter: ' + packet);
   this.socket.write(packet + '\r\n');
 };
 
